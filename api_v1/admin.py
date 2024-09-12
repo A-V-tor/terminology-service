9907a85aab09b4b3ab9b5bc2aaafef40befd6a97
A-V-tor <avtorca4@gmail.com>
2024-09-12 10:08:51 +0300

    add: добавить модели
    - Описание моделей
    - Настройка админки для моделей

---
+++ api_v1/admin.py
@@ -0,0 +1,73 @@
+from django.contrib import admin
+from .models import HandBook, HandBookElement, VersionHandBook
+
+
+class VersionHandBookInline(admin.TabularInline):
+    model = VersionHandBook
+    extra = 0
+
+
+
+class HandBookElementInline(admin.TabularInline):
+    model = HandBookElement
+    extra = 0
+
+
+class HandBookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'uniq_code', 'title', 'latest_version', 'latest_version_date')
+    list_filter = (
+        'uniq_code',
+        'title',
+    )
+    inlines = [VersionHandBookInline]
+
+    def latest_version(self, obj):
+        latest_version = obj.versions.order_by('-version_start_date').first()
+        return latest_version.version if latest_version else "N/A"
+
+    latest_version.short_description = 'Последняя версия'
+
+    def latest_version_date(self, obj):
+        latest_version = obj.versions.order_by('-version_start_date').first()
+        return latest_version.version_start_date if latest_version else "N/A"
+
+    latest_version_date.short_description = 'Дата начала действия'
+
+
+class VersionHandBookAdmin(admin.ModelAdmin):
+    list_display = ('handbook_uniq_code', 'handbook_title', 'version', 'version_start_date')
+    list_filter = (
+        'version',
+        'version_start_date',
+    )
+    list_select_related = ('handbook',)
+    inlines = [HandBookElementInline]
+
+    def handbook_uniq_code(self, obj):
+        return obj.handbook.uniq_code
+
+    handbook_uniq_code.short_description = 'Код справочника'
+
+    def handbook_title(self, obj):
+        return obj.handbook.title
+
+    handbook_title.short_description = 'Наименование справочника'
+
+    def handbook_display(self, obj):
+        return str(obj.handbook)
+
+    handbook_display.short_description = 'Справочник'
+
+
+
+class HandBookElementAdmin(admin.ModelAdmin):
+    list_display = ('uniq_code', 'value')
+    list_filter = (
+        'uniq_code',
+    )
+    list_select_related = ('version_hand_book',)
+
+
+admin.site.register(HandBook, HandBookAdmin)
+admin.site.register(VersionHandBook, VersionHandBookAdmin)
+admin.site.register(HandBookElement, HandBookElementAdmin)
---
+++ api_v1/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from datetime import datetime
+
+
+class HandBook(models.Model):
+    uniq_code = models.CharField(max_length=100, unique=True, null=False, verbose_name='Код')
+    title = models.TextField(max_length=300, null=False, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+
+    class Meta:
+        verbose_name = 'Справочник'
+        verbose_name_plural = 'Справочники'
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class VersionHandBook(models.Model):
+    version = models.CharField(max_length=50, null=False, verbose_name='Версия')
+    version_start_date = models.DateField(
+        default=datetime.now, verbose_name='Дата начала действия'
+    )
+
+    handbook = models.ForeignKey(
+        'HandBook', on_delete=models.CASCADE, null=False, related_name='versions', verbose_name="Справочник"
+    )
+
+    class Meta:
+        verbose_name = 'Версия справочник'
+        verbose_name_plural = 'Версия справочников'
+        constraints = [
+            models.UniqueConstraint(fields=['handbook', 'version'], name='unique_handbook_version'),
+            models.UniqueConstraint(fields=['handbook', 'version_start_date'],
+                                    name='unique_version_start_date_per_handbook')
+        ]
+
+    def __str__(self):
+        return f"{self.version}"
+
+
+class HandBookElement(models.Model):
+    version_hand_book = models.ForeignKey(
+        'VersionHandBook', on_delete=models.CASCADE, null=False, related_name='elements', verbose_name='Версия справочника'
+    )
+    uniq_code = models.CharField(max_length=100, null=False, verbose_name='Код')
+    value = models.TextField(max_length=300, null=False, verbose_name='значение')
+
+    class Meta:
+        verbose_name = 'Элемент справочника'
+        verbose_name_plural = 'Элементы справочников'
+        constraints = [
+            models.UniqueConstraint(fields=['version_hand_book', 'uniq_code'],
+                                    name='unique_version_hand_book_uniq_code')
+        ]
+
+    def __str__(self):
+        return f"{self.uniq_code}"
+
+
